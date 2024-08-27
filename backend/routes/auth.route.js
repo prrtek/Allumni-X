984a972c35527ffbbc4113b5e13678c1e6f388e6
prrtek <prrtek@outlook.com>
2024-08-27 10:17:30 +0530

    Added Features

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 import {
-  //   getMe,
+  getMe,
   login,
   logout,
   signup,
 } from "../controllers/auth.controller.js";
-// import { protectRoute } from "../middleware/protectRoute.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-// router.get("/me", protectRoute, getMe);
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDb.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 const PORT = process.env.PORT || 8000;
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
